ab7b2f3799c52346234b2d18b6d4472a23e5ce89
Victor Sales <vsales85@gmail.com>
2022-04-04 22:30:48 -0300

    up

--- desabilita.js
+++ desabilita.js
@@ -1,7 +1,12 @@
 function Enableddl(chkddl) {
-    var ddl = document.getElementById("DDL");
+    var ddl  = document.getElementById("DDL");
+    var ddl2  = document.getElementById("DDL2");
     ddl.disabled = chkddl.checked ? false : true;
+    ddl2.disabled = chkddl.checked ? false : true;
     if (!ddl.disabled) {
+    if (!ddl2.disabled) {    
         ddl.focus();
+        ddl2.focus();
+    }
     }
 }
