1238ee951f0a2aa7c661f8d7295ffd30b576ebfa
Victor Sales de Oliveira - Support Center - Quality <vsales85@gmail.com>
2022-04-08 14:26:18 -0300

    update

--- script.js
+++ script.js
@@ -2,12 +2,26 @@
 function calcular() {
     var salario = document.querySelector('#salario')
     var horat = document.querySelector('#horat')
-    var resultado = document.querySelector('span')
     var horase = document.querySelector('#horase')
     var het = document.querySelector('#het')
-    var dsr = document.querySelector('#dsr')
+    var DDL= document.querySelector('#DDL')
+    var DDL2= document.querySelector('#DDL2')
 
-    resultado.innerHTML = parseFloat(((salario.value) / parseFloat(horat.value)) * parseFloat(horase.value)) * parseFloat(het.value)
+   
+    
+    var result_extra =  document.querySelector('#extra')
+    var result_dsr = document.querySelector('#dsr')
+    
+    
+    
+    var hora_extra = (((salario.value) / (horat.value)) *(horase.value)) * (het.value)
+    var dias_uteis = DDL.value-DDL2.value
+    var dsr_extra = (hora_extra/dias_uteis)*DDL2.value
+  
 
 
+    result_extra.innerHTML=   (hora_extra);
+    result_dsr.innerHTML =  (dsr_extra);
+    
+    
 }
